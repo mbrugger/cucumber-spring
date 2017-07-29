e816648502d6728b1429568ef25898b778256adb
Martin Brugger <martin@brugger.eu>
2017-07-29 10:43:52 +0200

    removed pom.xml from gitignore, added logging to illustrate proper reset of data object

--- src/test/java/eu/brugger/martin/acceptance/world/CustomerData.java
+++ src/test/java/eu/brugger/martin/acceptance/world/CustomerData.java
@@ -1,5 +1,7 @@
 package eu.brugger.martin.acceptance.world;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -7,7 +9,13 @@ import org.springframework.stereotype.Component;
 @Scope("cucumber-glue")
 public class CustomerData {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerData.class);
+
     private String customer = "No Customer";
+
+    public CustomerData() {
+        LOGGER.info("Creating new customer data");
+    }
 
     public String getCustomer() {
         return this.customer;
